346a60ccc46a806a0be64ee6af3a6cdcd76f9519
mohammed qassim <07809310hd@gmail.com>
2026-07-30 21:24:34 +0300

    Clean build cache and force update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.diwaniyah_clean"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
